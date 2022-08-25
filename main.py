ed5ffb053f22336a8909f68fb242c7d0abc2048a
Omistaja <laura.erelaakso@metropolia.fi>
2022-08-25 22:40:13 +0300

    test

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 # Tehtävä 2.6
+import random
+print("Kolmenumeroinen numerolukon koodi:")
+print(random.randint(0, 9)) #Prints a number from 0 to 9 after picking a random number from 0 to 9
+RandomNumber = random.randint(0,9)
+RandomNumber1 = random.randint(0,9)
+print(RandomNumber)
+print(RandomNumber1)
+
+print(" ")
+
+import random
+print("Nelinumeroinen numerolukon koodi:")
+print(random.randint(1, 6))
+RandomNumber3 = random.randint(1, 6)
+RandomNumber4 = random.randint(1, 6)
+RandomNumber5 = random.randint(1, 6)
+print(RandomNumber3)
+print(RandomNumber4)
+print(RandomNumber5)
